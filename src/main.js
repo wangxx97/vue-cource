487097d7d203db39a65bbcaa8647272881f95be6
wangxixin <192390475@qq.coom>
2020-02-01 00:08:14 +0800

    12-学习render

--- src/main.js
+++ src/main.js
@@ -4,16 +4,67 @@ import App from './App.vue'
 import router from './router'
 import store from './store' /* 自动找store下的index.js */
 import Bus from './lib/bus'
+import CountTo from './components/count-to'
+
 if (process.env.NODE_ENV !== 'production') require('./mock')
 
 
 Vue.config.productionTip = false
 Vue.prototype.$bus = Bus
 
+const handleClick = event => {
+	console.log(event)
+	event.stopPropagation()
+}
+
+let list = [{name: 'lison'}, {name: 'lili'}]
+const getLiEleArr = (h) => {
+	return list.map((item, index) => h('li', {
+		on: {
+			'click': handleClick
+		},
+		key: `list_item_${index}`
+	}, item.name))
+}
+
 // vue根组件
 new Vue({
 	router,
 	store,
-
+	// render: h => {
+	//   return h(CountTo, {
+	//     'class': [],
+	//     attrs: {},
+	//     style: {},
+	//     props: {
+	//       endVal: 100
+	//     },
+	//     domProps: {
+	//       innerHTML: '123'
+	//     },
+	// on: {
+	//   'on-animation-end': (val) => {
+	//     console.log('animation end!')
+	//   }
+	// },
+	// nativeOn: {
+	//   'click': () => {
+	//     console.log('click!')
+	//   }
+	// },
+	// directives: [],
+	// scopedSlots: {},
+	// slot: '',
+	// key: '',
+	// ref: ''
+	// })
+	// }
+	// render: h => h('div', [
+	// 	h('ul', {
+	// 		on: {
+	// 			'click': handleClick
+	// 		}
+	// 	}, getLiEleArr(h))
+	// ])
 	render: h => h(App)
 }).$mount('#app')
---
+++ src/views/render-page.vue
@@ -0,0 +1,51 @@
+<template>
+	<div>
+		<!--		<ul @click="handleClick">-->
+		<!--			<li  @click.stop="handleClick" v-for="(item, index) in list" :key="`list_item_${index}`">{{item.name}}</li>-->
+		<!--		</ul>-->
+		<list :list="list" :render="renderFunc" :style="{color: 'red'}">
+			<count-to slot="aa" slot-scope="count" :end-val="count.number"></count-to>
+		</list>
+	</div>
+</template>
+
+
+<script>
+    import List from '../components/list'
+    import CountTo from '../components/count-to'
+
+    export default {
+        data() {
+            return {
+                list: [
+                    // {name: 'wxx'},
+                    // {name: 'gwh'},
+                    {number: 100},
+                    {number: 45}
+                ]
+            }
+        },
+        components: {
+            List,
+            CountTo
+        },
+        methods: {
+            renderFunc(h, number) {
+                return (
+                    <CountTo
+                nativeOn-click = {this.handleClick}
+                on-on-animation-end = {this.handleEnd}
+                endVal = {number}
+                style = {{color: 'pink'}}></CountTo>
+            )
+            },
+        },
+        handleClick(event) {
+            // console.log(event)
+        },
+        handleEnd() {
+            // console.log('end!')l
+        }
+    }
+
+</script>
