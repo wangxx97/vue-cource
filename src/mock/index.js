319ed591bb18b666cf782723b5a7e20a70c056e5
wangxixin <192390475@qq.coom>
2020-02-03 00:57:59 +0800

    18-tree文件目录1

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,20 +1,21 @@
 import Mock from 'mockjs'
-import {getUserInfo, login, authorization} from './response/user'
-import {getTableData} from './response/data'
-
+import { getUserInfo, login, authorization } from './response/user'
+import { getTableData, getFileList, getFolderList } from './response/data'
 const Random = Mock.Random
 
 Mock.mock(/\/getUserInfo/, 'post', getUserInfo)
 Mock.mock(/\/login/, 'post', login)
 Mock.mock(/\/authorization/, 'get', authorization)
 Mock.mock(/\/getTableData/, 'get', getTableData)
+Mock.mock(/\/getFileList/, 'get', getFileList)
+Mock.mock(/\/getFolderList/, 'get', getFolderList)
 
 Mock.setup({
 	timeout: 0
 })
 
 Random.extend({
-	fruit() {
+	fruit () {
 		const fruit = ['apple', 'peach', 'lemon']
 		return this.pick(fruit)
 	}
